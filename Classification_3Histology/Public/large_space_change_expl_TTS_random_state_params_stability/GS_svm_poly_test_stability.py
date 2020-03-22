10ee4fc7331bb5e1afdf5305ee13efdf995b5270
leonardoub <ubaldi.leonardo@gmail.com>
2020-03-22 17:46:01 +0100

    corr

--- Classification_3Histology/Public/large_space_change_expl_TTS_random_state_params_stability/GS_svm_poly_test_stability.py
+++ Classification_3Histology/Public/large_space_change_expl_TTS_random_state_params_stability/GS_svm_poly_test_stability.py
@@ -69,8 +69,6 @@ for i in range(1, 21):
        parameteres = [{'scaler':[MinMaxScaler()], 'red_dim':[PCA()], 'red_dim__n_components':list(n_features_to_test),
                      'clf__C': list(C_range), 'clf__gamma':['auto', 'scale']+list(gamma_range), 'clf__degree':[2, 3]},
                      {'scaler':[MinMaxScaler()], 'red_dim':[LinearDiscriminantAnalysis()], 'red_dim__n_components':[2],
-                     'clf__C': list(C_range), 'clf__gamma':['auto', 'scale']+list(gamma_range), 'clf__degree':[2, 3]},
-                     {'scaler':[MinMaxScaler()], 'red_dim':[None],
                      'clf__C': list(C_range), 'clf__gamma':['auto', 'scale']+list(gamma_range), 'clf__degree':[2, 3]}]
 
 
