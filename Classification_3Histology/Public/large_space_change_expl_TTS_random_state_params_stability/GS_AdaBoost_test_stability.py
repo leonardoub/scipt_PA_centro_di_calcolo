b2c51b68e838a250c042002d59d176821152aa5f
leonardoub <ubaldi.leonardo@gmail.com>
2020-04-05 13:25:26 +0200

    ada

--- Classification_3Histology/Public/large_space_change_expl_TTS_random_state_params_stability/GS_AdaBoost_test_stability.py
+++ Classification_3Histology/Public/large_space_change_expl_TTS_random_state_params_stability/GS_AdaBoost_test_stability.py
@@ -70,13 +70,13 @@ for i in range(1, 21):
 
 
        parameteres = [{'scaler':scalers_to_test, 'red_dim':[LinearDiscriminantAnalysis()], 'red_dim__n_components':[2], 
-                       'clf__n_estimators':n_estimators, 'clf__base_estimator__max_depth':depth, 
+                       'clf__n_estimators':n_estimators, 'base_estimator__max_depth':depth, 
                        'clf__learning_rate':lr, 'clf__algorithm':['SAMME', 'SAMME.R']},
                       {'scaler':scalers_to_test, 'red_dim':[PCA()], 'red_dim__n_components':n_features_to_test, 
-                       'clf__n_estimators':n_estimators, 'clf__base_estimator__max_depth':depth, 
+                       'clf__n_estimators':n_estimators, 'base_estimator__max_depth':depth, 
                        'clf__learning_rate':lr, 'clf__algorithm':['SAMME', 'SAMME.R']},
                        {'scaler':scalers_to_test, 'red_dim':[None], 
-                       'clf__n_estimators':n_estimators, 'clf__base_estimator__max_depth':depth, 
+                       'clf__n_estimators':n_estimators, 'base_estimator__max_depth':depth, 
                        'clf__learning_rate':lr, 'clf__algorithm':['SAMME', 'SAMME.R']}]
 
        grid = GridSearchCV(pipeline, param_grid=parameteres, cv=5, n_jobs=-1, verbose=1)
