c17211c78aa5ffc32a7c80221985e25ca8b8cf39
leonardoub <ubaldi.leonardo@gmail.com>
2020-04-07 11:43:01 +0200

    add corr tts

--- Classification_3Histology/Public/large_space_change_expl_all_random_state_params_stability/PCA/GS_RadiusNeighbors_test_stability.py
+++ Classification_3Histology/Public/large_space_change_expl_all_random_state_params_stability/PCA/GS_RadiusNeighbors_test_stability.py
@@ -67,7 +67,7 @@ for i in range(1, 21):
 
 
        parameteres = [{'scaler':scalers_to_test, 'red_dim':[PCA()], 'red_dim__n_components':n_features_to_test, 
-                       'red_dim__whiten':[False, True], 'red_dim__solver':['auto'], 'clf__radius':R, 
+                       'red_dim__whiten':[False, True], 'clf__radius':R, 
                        'clf__weights':['uniform', 'distance'], 'clf__algorithm':['auto', 'ball_tree', 'kd_tree', 'brute']}]
 
 
