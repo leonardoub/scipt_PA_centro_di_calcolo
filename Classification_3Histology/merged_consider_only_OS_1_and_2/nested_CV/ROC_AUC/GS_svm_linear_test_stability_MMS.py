0c13e9582897043392357d16c2705e4120710641
leonardoub <ubaldi.leonardo@gmail.com>
2020-05-19 15:47:15 +0200

    hist class 2 class os

---
+++ Classification_3Histology/merged_consider_only_OS_1_and_2/nested_CV/ROC_AUC/GS_svm_linear_test_stability_MMS.py
@@ -0,0 +1,55 @@
+#Cross Validation on SVM for classification
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy
+from sklearn.decomposition import PCA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.svm import SVC
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import GridSearchCV, KFold, cross_val_predict, cross_val_score, StratifiedKFold
+import load_data_3_class
+import save_output
+import nested_cv_3_classes
+
+name_clf = 'SVM_linear_MMS'
+
+
+#load data
+
+data, labels = load_data_3_class.function_load_data_3_class()
+
+#Scalers
+from sklearn.preprocessing import StandardScaler, RobustScaler, MinMaxScaler
+scalers_to_test = [RobustScaler(), MinMaxScaler()]
+
+df = pd.DataFrame()
+
+#Designate distributions to sample hyperparameters from 
+C_range = np.power(2, np.arange(-10, 11, dtype=float))
+n_features_to_test = [0.85, 0.9, 0.95]
+
+
+
+
+#SVM
+steps = [('scaler', MinMaxScaler()), ('red_dim', PCA()), ('clf', SVC(kernel='linear', probability=True))]
+
+pipeline = Pipeline(steps)
+
+parameteres = [{'scaler':[MinMaxScaler()], 'red_dim':[PCA(random_state=42)], 'red_dim__n_components':list(n_features_to_test), 
+                'clf__C':list(C_range), 'clf__class_weight':[None, 'balanced']},
+              {'scaler':[MinMaxScaler()], 'red_dim':[None], 'clf__C':list(C_range), 'clf__class_weight':[None, 'balanced']}]
+
+
+results = nested_cv_3_classes.function_nested_cv_3_classes(data, labels, pipeline, parameteres)
+
+#create folder and save
+
+save_output.function_save_output(results, name_clf)
+
+
+
---
+++ Classification_OS/Merged_OS_1_2/GS_KNeighborsClassifier_test_stability.py
@@ -0,0 +1,55 @@
+#Cross Validation on KNeighborsClassifier for classification
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy
+from sklearn.decomposition import PCA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import GridSearchCV, KFold, cross_val_predict, cross_val_score, StratifiedKFold
+import load_data_2_class
+import save_output
+import GSCV
+
+name_clf = 'KNeighborsClassifier'
+
+
+#load data
+
+X_train, y_train, X_test, y_test = load_data_2_class.function_load_data_2_class()
+
+
+#Scalers
+
+from sklearn.preprocessing import StandardScaler, RobustScaler, MinMaxScaler
+scalers_to_test = [StandardScaler(), RobustScaler(), MinMaxScaler(), None]
+
+df = pd.DataFrame()
+
+# Designate distributions to sample hyperparameters from 
+n_features_to_test = [0.85, 0.9, 0.95]
+k = np.arange(1,11)
+
+
+#KNeighborsClassifier
+steps = [('scaler', MinMaxScaler()), ('red_dim', PCA()), ('clf', KNeighborsClassifier())]
+
+pipeline = Pipeline(steps)
+
+parameteres = [{'scaler':scalers_to_test, 'red_dim':[PCA()], 'red_dim__n_components':n_features_to_test, 'clf__n_neighbors':k, 
+                     'clf__weights':['uniform', 'distance'], 'clf__algorithm':['auto', 'ball_tree', 'kd_tree', 'brute']},
+                     {'scaler':scalers_to_test, 'red_dim':[None], 'clf__n_neighbors':k, 
+                     'clf__weights':['uniform', 'distance'], 'clf__algorithm':['auto', 'ball_tree', 'kd_tree', 'brute']}]
+
+
+
+results = GSCV.function_GSCV(X_train, y_train, X_test, y_test, pipeline, parameteres)
+
+#create folder and save
+
+save_output.function_save_output(results, name_clf)
+
