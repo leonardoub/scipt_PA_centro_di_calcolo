1ec07a95401bfe3f045bca0af1827c7b883a0c8e
leonardoub <ubaldi.leonardo@gmail.com>
2020-06-03 16:51:39 +0200

    corr

--- Classification_OS/score_CV/default_HP/default_HP_train_PU_test_PA/save_output.py
+++ Classification_OS/score_CV/default_HP/default_HP_train_PU_test_PA/save_output.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd
 
-def function_save_output(final_data, scaler, name_clf, rs_outer_kf):
+def function_save_output(final_data, scaler, name_clf):
     outname = f'score_default_HP_2OS_PU_PA_{name_clf}_{scaler}.csv'
 
     outdir = f'/home/users/ubaldi/TESI_PA/score_ROC_AUC_default_HP/2OS/train_PU_test_PA/'
