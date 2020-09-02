21d59200e6ad2d136d052b2e26842e657e822cba
leonardoub <ubaldi.leonardo@gmail.com>
2020-09-02 17:08:00 +0200

    add

--- Classification_ST/nested_cv/ROC_AUC/GS_AdaBoost_test_stability.py
+++ Classification_ST/nested_cv/ROC_AUC/GS_AdaBoost_test_stability.py
@@ -5,7 +5,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy
 from sklearn.decomposition import PCA
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.feature_selection import SelectKBest, SelectPercentile
+from sklearn.feature_selection import f_classif, mutual_info_classif
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split
@@ -18,7 +19,7 @@ import save_output
 import nested_cv
 
 name = 'AdaBoost'
-dim_reduction = 'PCA'
+#dim_reduction = 'PCA'
 
 #load data
 
@@ -32,22 +33,28 @@ scalers_to_test = [StandardScaler(), RobustScaler(), MinMaxScaler(), None]
 # Designate distributions to sample hyperparameters from 
 n_features_to_test = [0.85, 0.9, 0.95]
 n_estimators = [10, 30, 50, 70, 100, 150]
-lr = [0.01, 0.1, 0.50, 1.0]
+lr = [0.001, 0.01, 0.1, 0.50, 1.0]
+
 
 #AdaBoost
-steps = [('scaler', MinMaxScaler()), ('red_dim', PCA()), ('clf', AdaBoostClassifier(random_state=503))]
+steps = [('scaler', MinMaxScaler()), ('red_dim', PCA()), ('clf', AdaBoostClassifier())]
 
 pipeline = Pipeline(steps)
 
-parameteres = [{'scaler':scalers_to_test, 'red_dim':[PCA(random_state=42)], 'red_dim__n_components':n_features_to_test,
-                     'clf__base_estimator': [DecisionTreeClassifier(max_depth = j) for j in range(1,6)],                     
-                     'clf__n_estimators':n_estimators, 'clf__learning_rate':lr, 'clf__algorithm':['SAMME', 'SAMME.R']}]
+parameteres = [{'scaler':scalers_to_test, 'red_dim':[PCA(random_state=42)], 'red_dim__n_components':n_features_to_test, 
+                'clf__n_estimators':n_estimators, 'clf__learning_rate':lr, 'clf__algorithm':['SAMME', 'SAMME.R']},
+                {'scaler':scalers_to_test, 'red_dim':[SelectPercentile(f_classif, percentile=10)], 
+                'clf__n_estimators':n_estimators, 'clf__learning_rate':lr, 'clf__algorithm':['SAMME', 'SAMME.R']},
+                {'scaler':scalers_to_test, 'red_dim':[SelectPercentile(mutual_info_classif, percentile=10)],
+                'clf__n_estimators':n_estimators, 'clf__learning_rate':lr, 'clf__algorithm':['SAMME', 'SAMME.R']},
+               {'scaler':scalers_to_test, 'red_dim':[None], 
+                'clf__n_estimators':n_estimators, 'clf__learning_rate':lr, 'clf__algorithm':['SAMME', 'SAMME.R']}]
 
 
-for j in range(1,6):
+for j in range(1,2):
     results, best_estimators_dict = nested_cv.function_nested_cv(public_data, public_labels, pipeline, parameteres, j*2)
 
     #create folder and save
 
-    save_output.function_save_output(results, dim_reduction, name, j*2)
+    save_output.function_save_output(results, name, j*2)
 
