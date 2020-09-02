aabc7f075bb706cfecdafe2b9848816ea498e675
leonardoub <ubaldi.leonardo@gmail.com>
2020-09-02 17:16:05 +0200

    add

--- Classification_ST/nested_cv/ROC_AUC/GS_svm_linear_comp_test_stability.py
+++ Classification_ST/nested_cv/ROC_AUC/GS_svm_linear_comp_test_stability.py
@@ -5,7 +5,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy
 from sklearn.decomposition import PCA
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.feature_selection import SelectKBest, SelectPercentile
+from sklearn.feature_selection import f_classif, mutual_info_classif
 from sklearn.svm import SVC
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split
@@ -17,7 +18,7 @@ import save_output
 import nested_cv
 
 name = 'svm_linear_comp'
-dim_reduction = 'PCA'
+#dim_reduction = 'PCA'
 
 #load data
 
@@ -28,24 +29,29 @@ public_data, public_labels = load_data.function_load_data()
 from sklearn.preprocessing import StandardScaler, RobustScaler, MinMaxScaler
 scalers_to_test = [StandardScaler(), RobustScaler(), MinMaxScaler()]
 
-# Designate distributions to sample hyperparameters from 
+#Designate distributions to sample hyperparameters from 
 C_range = np.power(2, np.arange(-10, 9, dtype=float))
 n_features_to_test = [0.85, 0.9, 0.95]
 
 
+
+
 #SVM
-steps = [('scaler', MinMaxScaler()), ('red_dim', PCA()), ('clf', SVC(kernel='linear', probability=True, random_state=503))]
+steps = [('scaler', MinMaxScaler()), ('red_dim', PCA()), ('clf', SVC(kernel='linear', probability=True))]
 
 pipeline = Pipeline(steps)
 
-
 parameteres = [{'scaler':[MinMaxScaler()], 'red_dim':[PCA(random_state=42)], 'red_dim__n_components':list(n_features_to_test), 
-              'clf__C': list(C_range), 'clf__class_weight':[None, 'balanced']}]
-
+                'clf__C':list(C_range), 'clf__class_weight':[None, 'balanced']},
+                {'scaler':[MinMaxScaler()], 'red_dim':[SelectPercentile(f_classif, percentile=10)],
+                'clf__C':list(C_range), 'clf__class_weight':[None, 'balanced']},
+                {'scaler':[MinMaxScaler()], 'red_dim':[SelectPercentile(mutual_info_classif, percentile=10)], 
+                'clf__C':list(C_range), 'clf__class_weight':[None, 'balanced']},
+              {'scaler':[MinMaxScaler()], 'red_dim':[None], 'clf__C':list(C_range), 'clf__class_weight':[None, 'balanced']}]
 
 for j in range(2,6):
     results, best_estimators_dict = nested_cv.function_nested_cv(public_data, public_labels, pipeline, parameteres, j*2)
 
     #create folder and save
 
-    save_output.function_save_output(results, dim_reduction, name, j*2)
+    save_output.function_save_output(results, name, j*2)
